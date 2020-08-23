c9750136fda1f3b72b7c1bd9226d407ecc2359fb
Vivian Oliveres <v.oliveres@gmail.com>
2020-08-23 19:24:38 +0200

    Fix proto package

--- src/main/scala/com/kensai/grpc/greeting/MainGreetingServer.scala
+++ src/main/scala/com/kensai/grpc/greeting/MainGreetingServer.scala
@@ -1,11 +1,11 @@
 package com.kensai.grpc.greeting
 
-import com.kensai.grpc.greeting.proto.greeting.{GreetingRequest, Person}
+import com.kensai.grpc.proto.greeting.{GreetingRequest, Person}
 
 object MainGreetingServer {
 
   def main(args: Array[String]): Unit = {
-    println("lol")
+    println("main")
 
     println(GreetingRequest().withPerson(Person().withFirstName("Vivian").withLastName("Oliveres")))
   }
